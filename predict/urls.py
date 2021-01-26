3b91dfe9b6dfb8c035643bb983627b78707800a3
Erick Hora <erick.hora@ufpe.br>
2021-01-26 13:46:22 -0300

    '

--- predict/urls.py
+++ predict/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'predict'
 
 urlpatterns = [
-    path('', views.predict, name='predict')
+    path('', views.predict, name='predict'),
+    path('predict/', views.predict_changes, name='submit_prediction')
 ]
